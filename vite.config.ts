04218d6039b786fac8ebb57a530d701aa449a0f8
dmitriy <korepanov.dmitriy.04@mail.ru>
2025-10-17 19:19:03 +0800

    try 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist', // Убедись, что это совпадает с vercel.json
+    emptyOutDir: true, // Очищает dist перед сборкой
+    rollupOptions: {
+      input: '/index.html' // Указываем точку входа
+    }
+  },
   server: {
     port: 5173
-  },
-  build: {
-    outDir: 'dist'
   }
 })
